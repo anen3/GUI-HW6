45f195e57028690b3d50ca005378a0c63a9a40fe
anen3 <alex_nguyen@student.uml.edu>
2019-11-14 15:44:18 -0500

    added comments to js, removed up console log

--- hw6.js
+++ hw6.js
@@ -1,13 +1,18 @@
+/*Alex Nguyen
+alex_nguyen@student.uml.edu
+UMass Lowell COMP4610 GUI Programming I
+Purpose of this webpage: Practicing js by implementing a dynamic table*/
+
 //modified the example from w3schools to add multiple rows using a loop https://www.w3schools.com/jsref/met_table_insertrow.asp 
-//figured out some form/dom manipulation from traversy dom crash course 4
-//generating a table using appendChild
+//some form/dom manipulation from traversy dom crash course 4
+
 var form = document.getElementById('addForm');
-var numRows = 0;
-// Form submit event
+var numRows = 0; // keeps track of the rows made in previous table. used to clear the table
+// Form submit event give input to function additem
 form.addEventListener('submit', addItem);
 
-// Add item
-function addItem(e){
+// Add item 
+function addItem(e){ 
 	var table = document.getElementById('mult');
 	var arr = [];
 	e.preventDefault();
@@ -41,9 +46,7 @@ function addItem(e){
 		alertNaN();
 		return;
 	}
-
-
-	//clear out the table 
+	//clear out previously made the table 
 	for(var i = 0; i <= numRows + 1; i++)
 		 document.getElementById("mult").deleteRow(-1);
 
@@ -56,101 +59,86 @@ function addItem(e){
 	}
 	if(newItem3 < newItem4) {
 		numRows = newItem4 - newItem3;
-		console.log('newItem4: ' + newItem4 + ', newItem3: ' +  newItem3);
 
 	}
 	else {
 		item3Big = 1;
 		numRows = newItem3 - newItem4;
-		console.log('newItem3: ' + newItem3 + ', newItem4: ' +  newItem4);
 	}
 	var a = [];
 	var table = document.getElementById("mult"); // var table and row set up to insert empty cell in first row
 	var row = table.insertRow(-1); // var table and row set up to insert empty cell in first row
 	a.push(row.insertCell(-1));
 	a[a.length -1].innerHTML = ""; // empty cell 
-	if(item1Big) {
+	if(item1Big) { //print table case 1 the first x input is greater than the second x input
 		for( i = newItem2; i <= newItem; i++)  // insert the entries for the first row
 		{
 			a.push(row.insertCell(-1));
-			console.log((typeof a[a.length-1]));
 			a[a.length -1].innerHTML = i;
 		}
-		if(item3Big) {
-			console.log('case 1');
+		if(item3Big) { //prints table case where y input 3 is greater than y input 4
 			for(var q = newItem4; q <= newItem3; q++) // prints j rows
 			{
 				var table = document.getElementById("mult");
 				var row = table.insertRow(-1);
 				a.push(row.insertCell(-1));
-				console.log((typeof a[a.length-1]));
 				a[a.length -1].innerHTML = q; // insert the y value into a cell
 
 				for(var i = newItem2; i <= newItem; i++) // adds in j entries per row
 				{
 				  a.push(row.insertCell(-1));
-				  console.log((typeof a[a.length-1]));
 				  a[a.length -1].innerHTML = q * i;
 				}
 			}
 		}
-		else {
-			console.log('case 2');
+		else { //prints table case where y input 3 is less than y input 4
 			for(var q = newItem3; q <= newItem4; q++) // prints j rows
 		  {
 			  var table = document.getElementById("mult");
 			  var row = table.insertRow(-1);
 			  a.push(row.insertCell(-1));
-			  console.log((typeof a[a.length-1]));
 			  a[a.length -1].innerHTML = q; // insert the y value into a cell
 			  
 			  for(var i = newItem2; i <= newItem; i++) // adds in j entries per row
 			  {
 				  a.push(row.insertCell(-1));
-				  console.log((typeof a[a.length-1]));
 				  a[a.length -1 ].innerHTML = q * i;
 			  }
 		  }
 		}
 	}
-	else { // when x range 2 is larger than x range 1
-		//print out x range in mult table ex: range 1 to 4 gives first  : 1 2 3 4 
+	else { // print table case when x input 2 is larger than x range 1
+
 		for(i = newItem; i <= newItem2; i++)  // insert the entries for the first row
 		  {
 		  a.push(row.insertCell(-1));
-		  console.log((typeof a[a.length-1]));
 		  a[a.length -1].innerHTML = i;
 		  }
-		if(item3Big) {
-			console.log('case 3');
-		  for(var q = newItem4; q <= newItem3; q++) // prints j rows
+		if(item3Big) { //prints table case where y input 3 is greater than y input 4
+		  for(var q = newItem4; q <= newItem3; q++) // prints rows that range from newItem4 to newItem3
 		  {
 			  var table = document.getElementById("mult");
 			  var row = table.insertRow(-1);
 			  a.push(row.insertCell(-1));
-			  console.log((typeof a[a.length-1]));
-			  a[a.length -1].innerHTML = q; // insert the y value into a cell
-			  for(var i = newItem; i <= newItem2; i++) // adds in j entries per row
+			  a[a.length -1].innerHTML = q; // insert the q value into a cell
+			  for(var i = newItem; i <= newItem2; i++) // adds in newItem2 entries per row
 			  {
 				  a.push(row.insertCell(-1));
-				  console.log((typeof a[a.length-1]));
 				  a[a.length -1].innerHTML = q * i;
 			  }
 		  }
 		}
 		else {
-			console.log('case 4');
-			for(q = newItem3; q <= newItem4; q++) // prints j rows
+			//print table case where y input 3 is less than y input 4
+			for(q = newItem3; q <= newItem4; q++) // prints rows that range from newItem3 to newItem4  
 		  {
 			  var table = document.getElementById("mult");
 			  var row = table.insertRow(-1);
 			  a.push(row.insertCell(-1));
-			  console.log((typeof a[a.length-1]));
-			  a[a.length -1].innerHTML = q; // insert the y value into a cell
-			  for(var i = newItem; i <= newItem2; i++) // adds in j entries per row
+			  a[a.length -1].innerHTML = q; // insert the q value into a cell
+			  for(var i = newItem; i <= newItem2; i++) // adds in entries from range newItem to newItem newItem2 
 			  {
 				  a.push(row.insertCell(-1));
-				  console.log((typeof a[a.length-1]));
 				  a[a.length -1].innerHTML = q * i;
 			  }
 		  }
